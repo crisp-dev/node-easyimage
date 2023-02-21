837710bc6784f7490dede56c35327fdd2bae697e
Valerian Saliou <valerian@valeriansaliou.name>
2023-02-21 14:50:15 +0100

    Restore tests

--- test/test.js
+++ test/test.js
@@ -52,186 +52,186 @@ describe('.info - ', function () {
 
 });
 
-// describe('.convert -', function () {
+describe('.convert -', function () {
 
-//     afterEach(function(done) {
-//         fs.unlink(__dirname + '/output/convert.png', function() {
-//             /* ignore any error unlinking */
-//             done();
-//         });
-//     });
+    afterEach(function(done) {
+        fs.unlink(__dirname + '/output/convert.png', function() {
+            /* ignore any error unlinking */
+            done();
+        });
+    });
 
-//     it('should convert an image format to another', function () {
+    it('should convert an image format to another', function () {
 
-//         return easyimg.convert({src:srcimg, dst: __dirname + '/output/convert.png', quality:10}).then(function (fileName) {
-//             fileName.should.be.a('string');
-//         });
+        return easyimg.convert({src:srcimg, dst: __dirname + '/output/convert.png', quality:10}).then(function (fileName) {
+            fileName.should.be.a('string');
+        });
 
-//     });
+    });
 
-// });
+});
 
-// describe('.rotate -', function () {
+describe('.rotate -', function () {
 
-//     afterEach(function(done) {
-//         fs.unlink(__dirname + '/output/rotated.jpg', function() {
-//             /* ignore any error unlinking */
-//             done();
-//         });
-//     });
+    afterEach(function(done) {
+        fs.unlink(__dirname + '/output/rotated.jpg', function() {
+            /* ignore any error unlinking */
+            done();
+        });
+    });
 
-//     it('should rotate the image by 90 degree', function () {
+    it('should rotate the image by 90 degree', function () {
 
-//         return easyimg.info(srcimg).then(function(info){
-//             info.should.have.property('width')
-//             info.should.have.property('height')
+        return easyimg.info(srcimg).then(function(info){
+            info.should.have.property('width')
+            info.should.have.property('height')
 
-//             // Mainly check only if width and height are swaped
-//             return easyimg.rotate({src:srcimg, dst: __dirname + '/output/rotated.jpg', degree: 90}).then(function (fileName) {
-//                 fileName.should.be.a('string');
-//             });
-//         })
-//     });
+            // Mainly check only if width and height are swaped
+            return easyimg.rotate({src:srcimg, dst: __dirname + '/output/rotated.jpg', degree: 90}).then(function (fileName) {
+                fileName.should.be.a('string');
+            });
+        })
+    });
 
-// });
+});
 
-// describe('.resize -', function () {
+describe('.resize -', function () {
 
-//     afterEach(function(done) {
-//         fs.unlink(__dirname + '/output/resize.jpg', function() {
-//             /* ignore any error unlinking */
-//             done();
-//         });
-//     });
+    afterEach(function(done) {
+        fs.unlink(__dirname + '/output/resize.jpg', function() {
+            /* ignore any error unlinking */
+            done();
+        });
+    });
 
-//     it('should resize an image with preserved aspect ratio', function () {
+    it('should resize an image with preserved aspect ratio', function () {
 
-//         return easyimg.resize({src:srcimg, dst: __dirname + '/output/resize.jpg', width:640, height:400}).then(function (fileName) {
-//             fileName.should.be.a('string');
-//         });
+        return easyimg.resize({src:srcimg, dst: __dirname + '/output/resize.jpg', width:640, height:400}).then(function (fileName) {
+            fileName.should.be.a('string');
+        });
 
-//     });
+    });
 
-//     it('should resize an image with ignored aspect ratio', function () {
+    it('should resize an image with ignored aspect ratio', function () {
 
-//         return easyimg.resize({src:srcimg, dst: __dirname + '/output/resize.jpg', width:640, height:400, ignoreAspectRatio: true}).then(function (fileName) {
-//             fileName.should.be.a('string');
-//         });
+        return easyimg.resize({src:srcimg, dst: __dirname + '/output/resize.jpg', width:640, height:400, ignoreAspectRatio: true}).then(function (fileName) {
+            fileName.should.be.a('string');
+        });
 
-//     });
+    });
 
-// });
+});
 
-// describe('.crop -', function () {
+describe('.crop -', function () {
 
-//     afterEach(function(done) {
-//         fs.unlink(__dirname + '/output/crop.jpg', function() {
-//             /* ignore any error unlinking */
-//             done();
-//         });
-//     });
+    afterEach(function(done) {
+        fs.unlink(__dirname + '/output/crop.jpg', function() {
+            /* ignore any error unlinking */
+            done();
+        });
+    });
 
-//     it('should crop an image', function () {
+    it('should crop an image', function () {
 
-//         return easyimg.crop({
-//             src:srcimg, dst: __dirname + '/output/crop.jpg',
-//             cropwidth:128, cropheight:128,
-//             gravity:'North',
-//             x:0, y:0
-//         }).then(function (fileName) {
-//             fileName.should.be.a('string');
-//         });
+        return easyimg.crop({
+            src:srcimg, dst: __dirname + '/output/crop.jpg',
+            cropwidth:128, cropheight:128,
+            gravity:'North',
+            x:0, y:0
+        }).then(function (fileName) {
+            fileName.should.be.a('string');
+        });
 
-//     });
+    });
 
-// });
+});
 
-// describe('.rescrop -', function () {
+describe('.rescrop -', function () {
 
-//     afterEach(function(done) {
-//         fs.unlink(__dirname + '/output/rescrop.jpg', function() {
-//             /* ignore any error unlinking */
-//             done();
-//         });
-//     });
+    afterEach(function(done) {
+        fs.unlink(__dirname + '/output/rescrop.jpg', function() {
+            /* ignore any error unlinking */
+            done();
+        });
+    });
 
-//     it('should resize and crop', function () {
+    it('should resize and crop', function () {
 
-//         return easyimg.rescrop({
-//             src:srcimg, dst: __dirname + '/output/rescrop.jpg',
-//             width:400, height:5400,
-//             cropwidth:100, cropheight:100,
-//             x:0, y:0
-//         }).then(function (fileName) {
-//             fileName.should.be.a('string');
-//         });
+        return easyimg.rescrop({
+            src:srcimg, dst: __dirname + '/output/rescrop.jpg',
+            width:400, height:5400,
+            cropwidth:100, cropheight:100,
+            x:0, y:0
+        }).then(function (fileName) {
+            fileName.should.be.a('string');
+        });
 
-//     });
+    });
 
-// });
+});
 
-// describe('.thumbnail -', function () {
+describe('.thumbnail -', function () {
 
-//     afterEach(function(done) {
-//         fs.unlink(__dirname + '/output/thumb nail.jpg', function() {
-//             /* ignore any error unlinking */
-//             done();
-//         });
-//     });
+    afterEach(function(done) {
+        fs.unlink(__dirname + '/output/thumb nail.jpg', function() {
+            /* ignore any error unlinking */
+            done();
+        });
+    });
 
-// 	//this.timeout(5000);
-//     it('should generate a thumbnail', function () {
+	//this.timeout(5000);
+    it('should generate a thumbnail', function () {
 
-//         return easyimg.thumbnail({
-//             src:srcimg, dst: __dirname + '/output/thumb nail.jpg',
-//             width:128, height:128,
-//             x:0, y:0
-//         }).then(function (fileName) {
-//             fileName.should.be.a('string');
-//         });
+        return easyimg.thumbnail({
+            src:srcimg, dst: __dirname + '/output/thumb nail.jpg',
+            width:128, height:128,
+            x:0, y:0
+        }).then(function (fileName) {
+            fileName.should.be.a('string');
+        });
 
-//     });
+    });
 
-// });
-
-
-// describe('.exec -', function () {
-
-//     afterEach(function(done) {
-//         fs.unlink(__dirname + '/output/hello kitty.gif', function() {
-//             /* ignore any error unlinking */
-//             done();
-//         });
-//     });
-
-//     it('should invoke `convert` command', function () {
-//         return easyimg.exec('convert '+ srcimg +' ./output/hello\\ kitty.gif').then(function (file) {
-//         });
-
-//     });
-
-// });
+});
 
 
-// describe('additionals', function () {
+describe('.exec -', function () {
 
-//     afterEach(function(done) {
-//         rimraf(__dirname + '/output/foo/', function() {
-//             done();
-//         });
-//     });
+    afterEach(function(done) {
+        fs.unlink(__dirname + '/output/hello kitty.gif', function() {
+            /* ignore any error unlinking */
+            done();
+        });
+    });
 
-//     //this.timeout(5000);
-//     it('should create directories if they do not exist', function () {
+    it('should invoke `convert` command', function () {
+        return easyimg.exec('convert '+ srcimg +' ./output/hello\\ kitty.gif').then(function (file) {
+        });
 
-//         return easyimg.thumbnail({
-//             src:srcimg, dst: __dirname + '/output/foo/bar/hey.jpg',
-//             width:128, height:128,
-//             x:0, y:0
-//         }).then(function (fileName) {
-//             fileName.should.be.a('string');
-//         });
+    });
 
-//     });
+});
 
-// });
+
+describe('additionals', function () {
+
+    afterEach(function(done) {
+        rimraf(__dirname + '/output/foo/', function() {
+            done();
+        });
+    });
+
+    //this.timeout(5000);
+    it('should create directories if they do not exist', function () {
+
+        return easyimg.thumbnail({
+            src:srcimg, dst: __dirname + '/output/foo/bar/hey.jpg',
+            width:128, height:128,
+            x:0, y:0
+        }).then(function (fileName) {
+            fileName.should.be.a('string');
+        });
+
+    });
+
+});
